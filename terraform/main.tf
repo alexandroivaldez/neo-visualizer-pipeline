683c946b3f65940721bb0abacc989521ef2c7ec1
Alexandro Valdez <alexandroivaldez@gmail.com>
2026-03-26 22:27:17 -0400

    feat: add S3 bucket and public access block via Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,16 @@ terraform {
 provider "aws" {
     region = var.aws_region
 }
+
+resource "aws_s3_bucket" "neo_data" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "neo_data" {
+  bucket = aws_s3_bucket.neo_data.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
